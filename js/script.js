7fb3447be1e7d2fef9d42bb55258bcf9567440cc
Raab Iane <raab.iane@gmail.com>
2023-09-21 10:16:20 -0300

    feat: Add sliding animation for projects

--- js/script.js
+++ js/script.js
@@ -15,3 +15,44 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
+
+// Initialize an object to track the completion of animations for specific projects
+let animationCompleted = {
+    projectOne: false,
+    projectTwo: false,
+    projectThree: false,
+    projectFour: false,
+    projectFive: false,
+};
+
+// Function to check if a project is visible and trigger animation if not completed
+function checkProjectVisibility(projectSelector, projectName, animationClass) {
+    const project = document.querySelector(projectSelector);
+    const windowHeight = window.innerHeight;
+    const rect = project.getBoundingClientRect();
+
+    if (!animationCompleted[projectName] && rect.top <= windowHeight / 2 && rect.bottom >= 0) {
+        // If the project is in the viewport, add the animation class
+        const projImageLeft = project.querySelector(animationClass);
+        projImageLeft.classList.add('animated');
+
+        // Mark the animation as completed for this project
+        animationCompleted[projectName] = true;
+    }
+}
+
+// Function to handle the scroll event
+function handleScroll() {
+    // Check the visibility of specific projects and trigger animations
+    checkProjectVisibility('.proj-one', 'projectOne', '.proj-image-left');
+    checkProjectVisibility('.proj-two', 'projectTwo', '.proj-image-right');
+    checkProjectVisibility('.proj-three', 'projectThree', '.proj-image-left');
+    checkProjectVisibility('.proj-four', 'projectFour', '.proj-image-right');
+    checkProjectVisibility('.proj-five', 'projectFive', '.proj-image-left');
+}
+
+// Add a scroll event listener to trigger animations on scroll
+window.addEventListener('scroll', handleScroll);
+
+// Add a load event listener to trigger animations on page load
+window.addEventListener('load', handleScroll);
